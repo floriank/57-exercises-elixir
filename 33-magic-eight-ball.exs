457162aef8be844fd4e9c2d4a01b559b1d6a3716
Florian Kraft <floriankraft@gmx.de>
2016-02-23 20:44:28 +0100

    initial (and only) version for the 8-ball

---
+++ 33-magic-eight-ball.exs
@@ -0,0 +1,27 @@
+defmodule MagicEightball do
+  @doc """
+  a magic 8-Ball that gives you an answer to your question. Not really.
+  """
+
+  @answers [
+    "Yes",
+    "No",
+    "Maybe",
+    "Ask again later!"
+  ]
+
+  import IO, only: [gets: 1, puts: 1]
+
+  @doc """
+  shake!
+  """
+  def shake! do
+    :random.seed(:os.timestamp)
+    gets("What is your question? ")
+    @answers
+    |> Enum.random
+    |> puts
+  end
+end
+
+MagicEightball.shake!
